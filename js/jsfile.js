ae8f73e9e7e9afdaf0e054c700d43a9621b5f707
Raju1-Ahmed <robiussaniraju@gmail.com>
2022-03-05 22:51:25 +0600

    type some java code

--- js/jsfile.js
+++ js/jsfile.js
@@ -1,1 +1,17 @@
-console.log('check');
+// console.log('check');
+const searchMobile = () =>{
+    const SearchField = document.getElementById('Search-Field');
+    const SearchFieldText = SearchField.value;
+    
+    const url =` https://openapi.programming-hero.com/api/phones?search=${SearchFieldText}`;
+    fetch(url)
+    .then(res =>res.json())
+    .then(data => displayMobiles(data.data))
+}
+
+const displayMobiles = mobiledata =>{
+    console.log(mobiledata);
+    mobiledata.forEach(mobilename => {
+        console.log(mobilename.phone_name);
+    });
+}
